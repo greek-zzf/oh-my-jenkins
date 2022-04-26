93673106874185b6f6ca3e3acaad7bcf0ec85e4e
Zhouzf <zhaofengzhou9@gmail.com>
2022-04-26 23:19:26 +0800

    Initialize oh-my-jenkins project

---
+++ src/main/java/com/greek/jenkins/OhMyJenkinsApplication.java
@@ -0,0 +1,14 @@
+package com.greek.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhMyJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OhMyJenkinsApplication.class, args);
+        System.out.println("smoke test");
+    }
+
+}
